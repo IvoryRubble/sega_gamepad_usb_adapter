68010ccfec5cfd2feeabd7489053c33008bebb9e
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2025-03-31 02:08:34 +0700

    Implemented debounce example

--- SegaGamepad_two_gamepads_keyboard_mode_debounce/src/main.cpp
+++ SegaGamepad_two_gamepads_keyboard_mode_debounce/src/main.cpp
@@ -5,48 +5,35 @@
 
 const unsigned int delayBeforeReadMicros = 10; 
 const unsigned int delayBeforeNextUpdateMicros = 2000;
-SegaGamepad segaGamepad(6, 1, 2, 3, 4, 5, 7, delayBeforeReadMicros, delayBeforeNextUpdateMicros);
+SegaGamepad segaGamepad1(A0, A1, A2, A3, 1, 0, 2, delayBeforeReadMicros, delayBeforeNextUpdateMicros);
+SegaGamepad segaGamepad2(9, 8, 7, 6, 5, 4, 3, delayBeforeReadMicros, delayBeforeNextUpdateMicros);
 
-ButtonDebounce modeButtonDebounce(50);
+const unsigned long debounceDelayMillis = 50;
+ButtonDebounce modeButtonDebounce1(debounceDelayMillis);
+ButtonDebounce modeButtonDebounce2(debounceDelayMillis);
 
 const int keysCount = 12;
-bool keys[keysCount];
-bool keysPrevious[keysCount];
+bool keys1[keysCount];
+bool keys2[keysCount];
+bool keysPrevious1[keysCount];
+bool keysPrevious2[keysCount];
 
-bool keysPressed[keysCount];
-bool keysReleased[keysCount];
-
-const char* keysPressedMessages[keysCount] = {
-  "segaGamepad.btnUp pressed",
-  "segaGamepad.btnDown pressed",
-  "segaGamepad.btnLeft pressed",
-  "segaGamepad.btnRight pressed",
-  "segaGamepad.btnA pressed",
-  "segaGamepad.btnB pressed",
-  "segaGamepad.btnC pressed",
-  "segaGamepad.btnX pressed",
-  "segaGamepad.btnY pressed",
-  "segaGamepad.btnZ pressed",
-  "segaGamepad.btnStart pressed",
-  "segaGamepad.btnMode pressed"
+const char* keysNames[keysCount] = {
+  "btnUp",
+  "btnDown",
+  "btnLeft",
+  "btnRight",
+  "btnA",
+  "btnB",
+  "btnC",
+  "btnX",
+  "btnY",
+  "btnZ",
+  "btnStart",
+  "btnMode"
 };
 
-const char* keysReleasedMessages[keysCount] = {
-  "segaGamepad.btnUp released",
-  "segaGamepad.btnDown released",
-  "segaGamepad.btnLeft released",
-  "segaGamepad.btnRight released",
-  "segaGamepad.btnA released",
-  "segaGamepad.btnB released",
-  "segaGamepad.btnC released",
-  "segaGamepad.btnX released",
-  "segaGamepad.btnY released",
-  "segaGamepad.btnZ released",
-  "segaGamepad.btnStart released",
-  "segaGamepad.btnMode released"
-};
-
-const uint8_t keysKeyboard[keysCount] = {
+const uint8_t keysKeyboard1[keysCount] = {
   'w',
   's',
   'a',
@@ -61,67 +48,85 @@ const uint8_t keysKeyboard[keysCount] = {
   '\\'
 };
 
+const uint8_t keysKeyboard2[keysCount] = {
+  't',
+  'g',
+  'f',
+  'h',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  ','
+};
+
 bool ledState = LOW;
 
 void setup() {
   Serial.begin(115200);
   Keyboard.begin();
-  segaGamepad.init();
-
-  pinMode(LED_BUILTIN, OUTPUT);
+  segaGamepad1.init();
+  segaGamepad2.init();
 }
 
 void loop() {
-  segaGamepad.update();
+  segaGamepad1.update();
+  segaGamepad2.update();
 
-  keys[0] = segaGamepad.btnUp;
-  keys[1] = segaGamepad.btnDown;
-  keys[2] = segaGamepad.btnLeft;
-  keys[3] = segaGamepad.btnRight;
-  keys[4] = segaGamepad.btnA;
-  keys[5] = segaGamepad.btnB;
-  keys[6] = segaGamepad.btnC;
-  keys[7] = segaGamepad.btnX;
-  keys[8] = segaGamepad.btnY;
-  keys[9] = segaGamepad.btnZ;
-  keys[10] = segaGamepad.btnStart;
-  keys[11] = segaGamepad.btnMode;
+  modeButtonDebounce1.updateState(segaGamepad1.btnMode);
+  modeButtonDebounce2.updateState(segaGamepad2.btnMode);
 
-  modeButtonDebounce.updateState(segaGamepad.btnMode);
+  keys1[0] = segaGamepad1.btnUp;
+  keys1[1] = segaGamepad1.btnDown;
+  keys1[2] = segaGamepad1.btnLeft;
+  keys1[3] = segaGamepad1.btnRight;
+  keys1[4] = segaGamepad1.btnA;
+  keys1[5] = segaGamepad1.btnB;
+  keys1[6] = segaGamepad1.btnC;
+  keys1[7] = segaGamepad1.btnX;
+  keys1[8] = segaGamepad1.btnY;
+  keys1[9] = segaGamepad1.btnZ;
+  keys1[10] = segaGamepad1.btnStart;
+  keys1[11] = modeButtonDebounce1.btnState;
+
+  keys2[0] = segaGamepad2.btnUp;
+  keys2[1] = segaGamepad2.btnDown;
+  keys2[2] = segaGamepad2.btnLeft;
+  keys2[3] = segaGamepad2.btnRight;
+  keys2[4] = segaGamepad2.btnA;
+  keys2[5] = segaGamepad2.btnB;
+  keys2[6] = segaGamepad2.btnC;
+  keys2[7] = segaGamepad2.btnX;
+  keys2[8] = segaGamepad2.btnY;
+  keys2[9] = segaGamepad2.btnZ;
+  keys2[10] = segaGamepad2.btnStart;
+  keys2[11] = modeButtonDebounce2.btnState;
 
   for (int i = 0; i < keysCount; i++) {
-    if (keys[i] && !keysPrevious[i]) {
-      keysPressed[i] = true;
-    } else {
-      keysPressed[i] = false;
+    if (keys1[i] && !keysPrevious1[i]) {
+      Serial.print(keysNames[i]); Serial.println(" pressed on gamepad 1");
+      Keyboard.press(keysKeyboard1[i]);
     }
 
-    if (!keys[i] && keysPrevious[i]) {
-      keysReleased[i] = true;
-    } else {
-      keysReleased[i] = false;
+    if (!keys1[i] && keysPrevious1[i]) {
+      Serial.print(keysNames[i]); Serial.println(" released on gamepad 1");
+      Keyboard.release(keysKeyboard1[i]);
     }
-  }
 
-  for (int i = 0; i < keysCount; i++) {
-    if (keysPressed[i]) {
-      // Serial.println(keysPressedMessages[i]);
-      // Keyboard.press(keysKeyboard[i]);
-      //ledState = !ledState;
+    if (keys2[i] && !keysPrevious2[i]) {
+      Serial.print(keysNames[i]); Serial.println(" pressed on gamepad 2");
+      Keyboard.press(keysKeyboard2[i]);
     }
-    if (keysReleased[i]) {
-      // Serial.println(keysReleasedMessages[i]);
-      // Keyboard.release(keysKeyboard[i]);
-      // ledState = !ledState;
+
+    if (!keys2[i] && keysPrevious2[i]) {
+      Serial.print(keysNames[i]); Serial.println(" released on gamepad 2");
+      Keyboard.release(keysKeyboard2[i]);
     }
   }
 
-  if (modeButtonDebounce.isBtnPressed) {
-     ledState = !ledState;
-  }
-  // ledState = modeButtonDebounce.btnState;
-  // ledState = keys[11];
-
-  digitalWrite(LED_BUILTIN, ledState);
-  memcpy(keysPrevious, keys, keysCount);
+  memcpy(keysPrevious1, keys1, keysCount);
+  memcpy(keysPrevious2, keys2, keysCount);
 }
